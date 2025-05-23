048bf4750ebadebffedb497326ee1f486f2677ac
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-05-23 17:30:09 -0300

    feat: returning error messages when something response error from firebase

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,12 +3,15 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<User?> register(String email, String password) async {
+  Future<User?> register(String email, String password, String name) async {
     try {
       final userCredential = await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+
+      await userCredential.user?.updateDisplayName(name);
+
       return userCredential.user;
     } catch (e) {
       return null;
@@ -23,8 +26,24 @@ class AuthService {
       );
       return userCredential.user;
     } catch (e) {
-      return null;
+      throw 'Erro ao fazer login: $e';
     }
+  }
+
+  Future<String> userUpdate(String email, String name, String password) async {
+    final user = _auth.currentUser;
+    try {
+      if (user != null) {
+        await user.updateDisplayName(name);
+        await user.verifyBeforeUpdateEmail(email);
+        await user.updatePassword(password);
+
+        return 'Perfil atualizado com sucesso';
+      }
+    } catch (e) {
+      throw 'Erro ao atualizar perfil: $e';
+    }
+    return 'Erro ao atualizar perfil';
   }
 
   Future<void> logout() async {
