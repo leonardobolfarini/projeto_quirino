84cddc52354150f141ac16f1f338c0cde23eaddf
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-05-24 17:21:30 -0300

    feat: Initialization of user stats when created

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_quirino/components/login.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:projeto_quirino/services/database_service.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final dbService = DatabaseService();
+  FirebaseAuth.instance.authStateChanges().listen((User? user) {
+    if (user != null) {
+      dbService.initializeUserStats();
+    }
+  });
+
   runApp(const MainApp());
 }
 
